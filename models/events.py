75116fae2702e52ec2a5f4b2012df056cdffb60e
reyshaznii <reyshazni@gmail.com>
2023-09-07 17:47:14 +0700

    working upload

--- models/events.py
+++ models/events.py
@@ -2,8 +2,9 @@ from typing import List
 from pydantic import BaseModel, EmailStr, Field
 
 class WebinarRegistrationRequest(BaseModel):
-    full_name: str = Field(..., example="John Doe", description="Nama lengkap peserta")
-    email: EmailStr = Field(..., example="john@example.com", description="Alamat email peserta")
-    phone_number: str = Field(..., example="1234567890", description="Nomor telepon peserta")
-    organization: str = Field(..., example="ABC Corp", description="Nama organisasi peserta")
-    webinar_id: int = Field(..., example=1, description="ID atau judul webinar")
+    full_name: str = Field(..., example="Jeko", description="Nama lengkap peserta")
+    email: EmailStr = Field(..., example="jeko@icee.com", description="Alamat email peserta")
+    phone_number: str = Field(..., example="08821673463", description="Nomor telepon peserta")
+    organization: str = Field(..., example="ICEE ITB", description="Nama organisasi peserta")
+    webinar_id: str = Field(..., example="event1", description="ID webinar")
+    url_bukti_pembayaran: str = Field(..., example="https://foto.jpg/", description="url")
--- routes/events.py
+++ routes/events.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 from models.events import WebinarRegistrationRequest
 import os
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, storage
 from google.cloud import firestore
 from utils import connect, open_worksheet, append_data
+from pytz import timezone
+from datetime import datetime
+import pytz
 
 credential_path = "sa.json"
 
@@ -13,14 +16,59 @@ cred = credentials.Certificate(credential_path)
 firebase_admin.initialize_app(cred)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
+# Inisialisasi Firebase Storage
+firebase_storage = storage.bucket(name="icee-2023.appspot.com")  # Ganti dengan nama bucket Firebase Anda
+
 db = firestore.Client(project='icee-2023')
 
 event_router = APIRouter(
     tags=["Events"]
 )
 
-@event_router.post("/register")
-async def register(request: WebinarRegistrationRequest):
+from pytz import timezone
+from datetime import datetime
+import pytz
+
+@event_router.post("/uploadFile")
+async def upload_file(file: UploadFile):
+    allowed_formats = {'pdf', 'jpeg', 'jpg', 'png'}
+    max_file_size = 5 * 1024 * 1024  # 5 MB
+
+    try:
+        # Validasi format file
+        file_format = file.filename.split('.')[-1].lower()
+        if file_format not in allowed_formats:
+            raise HTTPException(status_code=400, detail="Format file tidak didukung")
+
+        # Validasi ukuran file
+        file_size = file.file.seek(0, 2)
+        if file_size > max_file_size:
+            raise HTTPException(status_code=400, detail="Ukuran file terlalu besar")
+
+        # Kembali ke awal file
+        file.file.seek(0)
+
+        # Membuat nama file baru dengan menggunakan datetime
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        new_filename = f"{current_time}.{file.filename}"
+
+        # Upload file ke Firebase Storage dengan nama baru
+        blob = firebase_storage.blob(new_filename)
+        blob.upload_from_file(file.file)
+
+        # Dapatkan URL file yang diupload
+        file_url = f"https://storage.googleapis.com/{firebase_storage.name}/{blob.name}"
+
+        # Mengembalikan respons sukses dengan URL file yang diupload
+        return {"message": "success", "file_url": file_url}
+
+    except HTTPException as http_exception:
+        return JSONResponse(status_code=http_exception.status_code, content={"message": http_exception.detail})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": f"Terjadi kesalahan: {str(e)}"})
+
+@event_router.post("/uploadData")
+async def upload_data(request: WebinarRegistrationRequest):
     try:
         # Lakukan validasi atau pemrosesan data sesuai kebutuhan
 
@@ -28,13 +76,21 @@ async def register(request: WebinarRegistrationRequest):
         credentials_file = "sa.json"
         spreadsheet = connect(credentials_file)
 
-        worksheet = open_worksheet(spreadsheet, "[Testing] Registrant", "event1")  # Sesuaikan nama worksheet sesuai kebutuhan
+        worksheet = open_worksheet(spreadsheet, "[Testing] Registrant", request.webinar_id)  # Sesuaikan nama worksheet sesuai kebutuhan
 
-        data_to_append = [request.full_name, request.email, request.phone_number, request.organization, request.webinar_id]
+        # Dapatkan waktu saat ini dalam timezone "Asia/Jakarta" (WIB)
+        jakarta_timezone = pytz.timezone("Asia/Jakarta")
+        current_datetime_wib = datetime.now(jakarta_timezone)
+
+        # Format waktu ke dalam string sesuai dengan format "9/7/2023 16:24:10"
+        formatted_datetime = current_datetime_wib.strftime("%m/%d/%Y %H:%M:%S")
+
+        # Tambahkan data ke dalam worksheet, termasuk waktu dalam format yang diinginkan dan URL file
+        data_to_append = [formatted_datetime, request.full_name, request.email, request.phone_number, request.organization, request.url_bukti_pembayaran]
         append_data(worksheet, data_to_append)
 
         # Mengembalikan respons sukses
-        return {"message": "success", "data": request.dict()}
+        return {"message": "success"}
 
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": str(e), "data": {}})
