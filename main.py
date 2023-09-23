03a66e5d5466be1e7ec3b29d3a8b332aa741d360
reyshaznii <reyshazni@gmail.com>
2023-09-23 19:40:07 +0700

    fix: cors

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routes.events import event_router, admin_router, asset_router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(admin_router, prefix="/admin")
 app.include_router(event_router, prefix="/register")
