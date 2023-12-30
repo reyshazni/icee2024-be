9267b4d5ae4142b045e6fcf1570b0890977c368c
reyshaznii <reyshazni@gmail.com>
2023-12-30 16:27:31 +0700

    feat: seminar registrant

--- models/events.py
+++ models/events.py
@@ -1,5 +1,5 @@
-from typing import List
-from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional
+from pydantic import BaseModel, EmailStr, Field, validator, root_validator
 from enum import Enum
 
 class NonWorkshopEnum(str, Enum):
@@ -12,21 +12,35 @@ class FileTypeEnum(str, Enum):
     bukti_follow = "bukti_follow"
     ktm = "ktm"
 
-class NonWorkshopRegistrationRequest(BaseModel):
-    event_name: str = Field(..., example="event1")
-    full_name: NonWorkshopEnum = Field(..., example="conference")
-    email: EmailStr = Field(..., example="jeko@icee.com")
-    phone_number: str = Field(..., example="08821673463")
-    institution: str = Field(..., example="Institut Teknologi Bandung")
-    profession: str = Field(..., example="Mahasiswa")
-    address: str = Field(..., example="Jalan Gatot Subroto 1A, Jakarta")
-    url_bukti_pembayaran: str = Field(..., example="https://storage.googleapis.com/bucket/file.jpg")
+class JenisPesertaEnum(str, Enum):
+    umum = "umum"
+    tpb = "tpb"
+    hms = "hms"
 
-class WorkshopRegistrationRequest(BaseModel):
-    full_name: str = Field(..., example="Jeko")
-    email: EmailStr = Field(..., example="jeko@icee.com")
-    phone_number: str = Field(..., example="08821673463")
-    institution: str = Field(..., example="Institut Teknologi Bandung")
-    profession: str = Field(..., example="Mahasiswa")
-    address: str = Field(..., example="Jalan Gatot Subroto 1A, Jakarta")
-    url_bukti_follow: str = Field(..., example="https://storage.googleapis.com/bucket/file.jpg")
+class DataDiri(BaseModel):
+    nama_lengkap: str = Field(..., example="Reinhart Jericho")
+    jenis_peserta: JenisPesertaEnum = Field(..., example=JenisPesertaEnum.umum)
+    pekerjaan: str = Field(..., example="Software Engineer")
+    institusi: str = Field(..., example="PT Teknologi Maju")
+    nim: Optional[str] = Field(None, example="12345678")
+    alamat: str = Field(..., example="Jl. Merdeka No.10, Jakarta")
+    no_telp: str = Field(..., example="081234567890")
+    email: EmailStr = Field(..., example="reinhart@example.com")
+
+    @validator('no_telp')
+    def validate_no_telp(cls, v):
+        if not v.startswith("0"):
+            raise ValueError("Nomor telepon harus diawali dengan '0'")
+        return v
+
+    @root_validator
+    def validate_nim(cls, values):
+        jenis_peserta = values.get('jenis_peserta')
+        nim = values.get('nim')
+        if jenis_peserta != JenisPesertaEnum.umum and not nim:
+            raise ValueError("NIM harus diisi untuk jenis peserta selain 'umum'")
+        return values
+
+class SeminarRequest(BaseModel):
+    data_diri: List[DataDiri]
+    url_bukti_pembayaran: str = Field(..., example="https://storage.googleapis.com/bucket/file.jpg")
